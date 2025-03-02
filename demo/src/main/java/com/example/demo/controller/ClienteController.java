daf9f59de58d5bdf54f8af6ac4d29e35853a8dbe
Valeria Arenas <126576318+ValeriaArenasB@users.noreply.github.com>
2025-03-02 15:56:39 -0500

    feat: basic CRUD Cliente functionality and css file for forms

---
+++ demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -0,0 +1,65 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.entity.Cliente;
+import com.example.demo.service.ClienteService;
+
+@RequestMapping("/cliente")
+@Controller
+public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping("/all")
+    public String mostrarClientes(Model model) {
+        model.addAttribute("clientes", clienteService.searchAll());
+        return "mostrar_todos_clientes";
+    }
+
+    @GetMapping("/find")
+    public String mostrarInfoCliente(Model model, @RequestParam("cedula") String cedula) {
+        Cliente cliente = clienteService.searchByCedula(cedula);
+        if (cliente != null) {
+            model.addAttribute("cliente", cliente);
+        } else {
+            //implementar not found exception pero con Strings
+        }
+        return "mostrar_cliente";
+    }
+
+    @GetMapping("/add")
+    public String mostrarFormularioCrear(Model model) {
+        Cliente cliente = new Cliente("", "", "", "");
+        model.addAttribute("cliente", cliente);
+        return "crear_cliente";
+    }
+
+    @PostMapping("/agregar")
+    public String agregarCliente(@ModelAttribute("cliente") Cliente cliente) {
+        clienteService.add(cliente);
+        return "redirect:/cliente/all";
+    }
+
+    @GetMapping("/delete/{cedula}")
+    public String eliminarCliente(@PathVariable("cedula") String cedula) {
+        clienteService.deleteByCedula(cedula);
+        return "redirect:/cliente/all";
+    }
+
+    @GetMapping("/update/{cedula}")
+    public String mostrarFormularioUpdate(@PathVariable("cedula") String cedula, Model model) {
+        model.addAttribute("cliente", clienteService.searchByCedula(cedula));
+        return "modificar_cliente";
+    }
+
+    @PostMapping("/update/{cedula}")
+    public String updateCliente(@PathVariable("cedula") String cedula, @ModelAttribute("cliente") Cliente cliente) {
+        clienteService.update(cliente);
+        return "redirect:/cliente/all";
+    }
+} 
---
+++ demo/src/main/java/com/example/demo/entity/Cliente.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    private String cedula;
+    private String nombre;
+    private String correo;
+    private String celular;
+    private List<Mascota> mascotas;
+
+    public Cliente(String cedula, String nombre, String correo, String celular) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.celular = celular;
+        this.mascotas = new ArrayList<>();
+    }
+
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public List<Mascota> getMascotas() {
+        return mascotas;
+    }
+
+    public void setMascotas(List<Mascota> mascotas) {
+        this.mascotas = mascotas;
+    }
+
+    
+} 
