b52c6b178e5ba26fcac750ad777eb6cc44fef101
Juan Canon <canonjp@javeriana.edu.co>
2025-03-18 15:01:43 -0500

    feature: added Droga emtity

--- demo/src/main/java/com/example/demo/repository/TratamientoRepository.java
+++ demo/src/main/java/com/example/demo/repository/TratamientoRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,11 +11,7 @@ import com.example.demo.model.Tratamiento;
 @Repository
 public interface TratamientoRepository  extends JpaRepository<Tratamiento, Long> {
     // Métodos personalizados para buscar un tratamiento por cualquier atributo
-    Tratamiento findByNombreDroga(String nombreDroga);
-    Tratamiento findByPrecioCompra(float precioCompra);
-    Tratamiento findByPrecioVenta(float precioVenta);
-    Tratamiento findByUnidadesDisponibles(float unidadesDisponibles);
-    Tratamiento findByUnidadesVendidas(int unidadesVendidas);
+    public Tratamiento findByFecha(Date fecha);
     void deleteById(Long id);
     
 }
